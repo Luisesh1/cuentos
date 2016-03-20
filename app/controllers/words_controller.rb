8e0e67f83981d833be994aec5c3449be160a4d98
luisesh1 <luisesh2@hotmail.com>
2016-03-20 15:09:09 -0600

    icon errores words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,14 @@
 class WordsController < ApplicationController
   def index
   	@words = Word.all
-  	
+  	@tuplas=nil
+  	unless params[:search].nil?
+  		cadena=params[:search]
+  		pal=cadena.split(" ")
+  		pal.each do |p|
+  			@tuplas=Word.where(palabra:p.strip)
+  		end
+  	end
   end
 
   def create
